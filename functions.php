7a9f89b3d9f66feac9a82871002d7a551e418e72
Brenon Paul <bp.brenon@gmail.com>
2019-08-21 09:29:43 -0300

    mudancas na calculadora

--- functions.php
+++ functions.php
@@ -17,7 +17,6 @@ function calculos($mascara, $primeiroOcteto, $segundoOcteto, $terceiroOcteto, $u
 
 	$intervalo = 256 - $mascaradecimal;
 	$qntdsubrede = 256 / $intervalo;
-
         //quantidade subredes definida
 
 	$qntendsubrede = $intervalo;
@@ -25,29 +24,6 @@ function calculos($mascara, $primeiroOcteto, $segundoOcteto, $terceiroOcteto, $u
 
 	$hosts = $qntendsubrede - 2;
         //quantidade hosts por subredes definida
-
-	$subrede = (int)($ultimoOcteto / $intervalo);
-        //subrede que o ip se encontra
-
-	$primeiroEnderecoRede = $subrede * $intervalo;
-
-	$prihost = $primeiroEnderecoRede + 1;
-	$primeirohost = $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $prihost . "/" . $mascara;
-        //primeiro endereço de host da subrede
-
-	$broadcast = $primeiroEnderecoRede + $intervalo - 1;
-	$ipbroadcast = $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $broadcast . "/" . $mascara;
-        //endereço BroadCast
-
-	$IpEnderecoDeRede = $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $primeiroEnderecoRede . "/" . $mascara;
-        //endereço de Rede
-
-	$ulthost = ($broadcast - 1);
-	$ultimohost = $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $ulthost . "/" . $mascara;
-        //ultimo endereço host da subrede
-
-
-        //broadcast da subrede
 
         //classe que o ip pertence
 	if ($primeiroOcteto <= 126 and $primeiroOcteto >= 1) {
@@ -80,124 +56,44 @@ function calculos($mascara, $primeiroOcteto, $segundoOcteto, $terceiroOcteto, $u
 		$o = "Público";
 	}
 
-	echo "<p><strong> Quantidade de subredes:</strong> $qntdsubrede</p>";
-	for ($i=1; $i <= $qntdsubrede ; $i++) { 
-		$enderecosRede = -$intervalo*$i;
-		$enderecosRede = $intervalo - $intervalo * $i;
-		$enderecosRede = $enderecosRede *-1;
-		$cont = $i;
-		$enderecosBroad = $intervalo * $i -1;
-		$primeirosHosts = $enderecosRede +1;
-		$ultimosHost = $enderecosBroad -1;
-		$ipInformado = $ip;
+	if ($primeiroOcteto>255 or $segundoOcteto>255 or $terceiroOcteto>255 or$ultimoOcteto>255 or $mascara> 32) {
+		echo "<h1 style='color: red; border: none'>Ip Inválido</h1>";
+	}else{
 
-		if ($enderecosRede < $ultimoOcteto and $ultimoOcteto < $enderecosBroad) {
-			echo "<p><strong> $cont ° subrede:</strong></p>";
-			echo "<p><u>rede:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$enderecosRede/$mascara</strong></p>";
-			echo "<p><u>broadcast:</u><strong> $primeiroOcteto. $segundoOcteto.$terceiroOcteto.$enderecosBroad/$mascara</strong></p>";
-			echo "<p><u>primeiro host:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$primeirosHosts/$mascara</strong></p>";
-			echo "<p><u>último host:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$ultimosHost/$mascara</strong></p>";
-		}else{
-			echo "<p><strong> $cont ° subrede:</strong></p>";
-			echo "<p><u>rede:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$enderecosRede/$mascara</p>";
-			echo "<p><u>broadcast:</u> $primeiroOcteto. $segundoOcteto.$terceiroOcteto.$enderecosBroad/$mascara</p>";
-			echo "<p><u>primeiro host:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$primeirosHosts/$mascara </p>";
-			echo "<p><u>último host:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$ultimosHost/$mascara </p>";
-		}
+		echo "<p><strong> Quantidade de subredes:</strong> $qntdsubrede</p>";
+		echo "<p><strong>Quantidade de hosts em cada subrede:</strong> $hosts </p>";
+		for ($i=1; $i <= $qntdsubrede ; $i++) { 
+			$enderecosRede = -$intervalo*$i;
+			$enderecosRede = $intervalo - $intervalo * $i;
+			$enderecosRede = $enderecosRede *-1;
+			$cont = $i;
+			$enderecosBroad = $intervalo * $i -1;
+			$primeirosHosts = $enderecosRede +1;
+			$ultimosHost = $enderecosBroad -1;
+			$ipInformado = $ip;
 
-		
-	}
-
-	echo "<p><strong>Quantidade de hosts em cada subrede:</strong> $hosts </p>";
-	echo "<p><strong>Máscara decimal:</strong> $mascaraA</p>";
-	echo "<p><strong>Classe da rede:</strong> $classe</strong></p>";
-	echo "<p><strong>Ip de rede:</strong> ". $o. "</p>";
-	
-	
-	/*echo "<p>A rede possui $qntendsubrede endereços por subrede </p>";
-	echo "<p>A rede possui o $primeirohost como primeiro host </p>";
-	echo "<p>A rede possui o $ultimohost como ultimo host </p>";
-	
-	
-	echo "<p>Endereços Disponíveis:</p>";
-
-	
-	
-	
-
-	$enderecoRede = $qntdsubrede * $intervalo -1;
-	echo "broadcast $enderecoRede";
-
-
-
-	if ($mascara == 24) {
-
-		for ($i=1; $i < $intervalo-1 ; $i++) { 
-			$Iphosts= $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $i . "/" . $mascara;
-
-			if ($ip == $Iphosts) {
-				echo "<p><strong>$Iphosts</strong></p>";
+			if ($enderecosRede < $ultimoOcteto and $ultimoOcteto < $enderecosBroad) {
+				echo "<p><strong> $cont ° subrede:</strong></p>";
+				echo "<p><u>rede:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$enderecosRede/$mascara</strong></p>";
+				echo "<p><u>broadcast:</u><strong> $primeiroOcteto. $segundoOcteto.$terceiroOcteto.$enderecosBroad/$mascara</strong></p>";
+				echo "<p><u>primeiro host:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$primeirosHosts/$mascara</strong></p>";
+				echo "<p><u>último host:</u><strong> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$ultimosHost/$mascara</strong></p>";
 			}else{
-				echo"<p>$Iphosts</p>";
+				echo "<p><strong> $cont ° subrede:</strong></p>";
+				echo "<p><u>rede:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$enderecosRede/$mascara</p>";
+				echo "<p><u>broadcast:</u> $primeiroOcteto. $segundoOcteto.$terceiroOcteto.$enderecosBroad/$mascara</p>";
+				echo "<p><u>primeiro host:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$primeirosHosts/$mascara </p>";
+				echo "<p><u>último host:</u> $primeiroOcteto.$segundoOcteto.$terceiroOcteto.$ultimosHost/$mascara </p>";
 			}
+
 		}
 
-	/*
+		echo "<p><strong>Máscara decimal:</strong> $mascaraA</p>";
+		echo "<p><strong>Classe da rede:</strong> $classe</strong></p>";
+		echo "<p><strong>Ip de rede:</strong> ". $o. "</p>";
 
-	}elseif ($mascara == 25) {
-		for ($j= 1; $j <= 1 ; $j++) {
-			for ($i=1; $i < $limite = ($intervalo -1); $i++) { 
-				$Iphosts= $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $i . "/" . $mascara;
-				if ($ip == $Iphosts) {
-					echo "<p><strong>$Iphosts</strong></p>";
-				}else{
-					echo"<p>$Iphosts</p>";
-				}
-			}
-		}
-		for ($j= 1; $j <= 1 ; $j++) { 
-			for ($i=129; $i < ($qntdsubrede * $intervalo -1); $i++) { 
-				$Iphosts= $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $i . "/" . $mascara;
-				if ($ip == $Iphosts) {
-
-
-					$seghost = $primeiroEnderecoRede + 1;
-					$primeirohost = $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $seghost . "/" . $mascara;
-
-
-					echo "<p><strong>$Iphosts</strong></p>";
-				}else{
-					echo"<p>$Iphosts</p>";
-				}
-			}
-		}
 	}
 
-	
-	}elseif ($mascara == 25) {
-			for ($k=1; $k <= $qntdsubrede ; $k++) { 
-				for ($i=1; $i < $limite = ($intervalo * $k) -1; $i++) { 
-					$Iphosts= $primeiroOcteto. "." . $segundoOcteto . "." . $terceiroOcteto . "." . $i . "/" . $mascara;
-						if ($ip == $Iphosts) {
-							echo "<p><strong>$Iphosts</strong></p>";
-						}else{
-							echo"<p>$Iphosts</p>";
-						}
-				}
-			}
-	}
-	
-*/
-
-
-
-
-
-
-//        echo "IP = ".$ip.". A rede possui ".$qntdsubrede." subredes, ".$qntendsubrede." endereços por subrede e ".$hosts." hosts por subrede.
-//         O primeiro endereço de host onde o endereço de IP se encontra é ".$primeirohost.", o último é ". $ultimohost." e
-//         o endereço de broadcast é ".$ipbroadcast.". A máscara de rede em decimal é ".$mascaraA.". A classe do ip é ".$classe." e
-//         é um endereço ".$o.".";
 }
 
 calculos($mascara,$primeiroOcteto,$segundoOcteto, $terceiroOcteto, $ultimoOcteto, $bits);
--- index.php
+++ index.php
@@ -3,32 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>JobProgramathions</title>
-
     <link rel="stylesheet" href="estilo.css">
     <link rel="stylesheet" type="text/css" href="bootstrap.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-     <script>
-    $(document).ready(function(){
-        $("#botao").click(function () {
-            var pri = $('#pri').val();
-            var seg = $('#seg').val();
-            var ter = $('#ter').val();
-            var qua = $('#ult').val();
-            var mas = $('#mascara').val();
-            $.post("functions.php",
-            {
-                primeiroOcteto : pri, segundoOcteto : seg, terceiroOcteto : ter, ultimoOcteto : qua, mascara : mas
-            },
-            function (data) {
-                $("#conteudo").html(data);
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#botao").click(function () {
+                var pri = $('#pri').val();
+                var seg = $('#seg').val();
+                var ter = $('#ter').val();
+                var qua = $('#ult').val();
+                var mas = $('#mascara').val();
+                $.post("functions.php",
+                {
+                    primeiroOcteto : pri, segundoOcteto : seg, terceiroOcteto : ter, ultimoOcteto : qua, mascara : mas
+                },
+                function (data) {
+                    $("#conteudo").html(data);
+                });
             });
         });
-    });
-
     </script>
-
 </head>
 <body id="body">
 
@@ -36,22 +31,16 @@
 
     <div id="input">
         <form method="post" id="formulario">
-                <input type="number" name="primeiroOcteto" id="pri" class=" inputs">.
-                <input type="number" name="segundoOcteto" id="seg" class=" inputs">.
-                <input type="number" name="terceiroOcteto" id="ter" class=" inputs">.
-                <input type="number" name="ultimoOcteto" id="ult" class=" inputs">/
-                <input type="number" min="24" max="32" name="mascara" id="mascara">
-                <input type="button" id="botao" value="enviar">
+            <input type="number" name="primeiroOcteto" id="pri" class=" inputs">.
+            <input type="number" name="segundoOcteto" id="seg" class=" inputs">.
+            <input type="number" name="terceiroOcteto" id="ter" class=" inputs">.
+            <input type="number" name="ultimoOcteto" id="ult" class=" inputs">/
+            <input type="number" min="24" max="32" name="mascara" id="mascara">
+            <input type="button" id="botao" value="enviar">
         </form>
     </div>
     <br>
     <div id="conteudo">
-        
-
-
-
-        
     </div>
-
 </body>
 </html>
